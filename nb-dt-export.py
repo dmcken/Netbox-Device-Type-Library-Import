3caf5c2706ae94c6db86a19c2b7b79e437a32cbd
David Mc Ken <david.mcken@gmail.com>
2023-03-17 11:47:54 -0400

    Add nb-dt-export.py Scaffolding for export script added.

---
+++ nb-dt-export.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+Netbox device-type export.
+
+Fetches device and module types from netbox and adds it to the repo.
+
+'''
+
+# System imports
+import collections
+import datetime
+import glob
+import os
+import re
+
+# External imports
+import pynetbox
+import yaml
+
+# Local imports
+import settings
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
